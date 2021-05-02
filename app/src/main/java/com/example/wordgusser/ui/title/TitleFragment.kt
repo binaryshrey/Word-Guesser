b406fa3c133add25096b987e4c6cc628eb89f8f3
binaryshrey <shreyansh.saurabh2016@vitstudent.ac.in>
2021-05-02 11:46:20 +0530

    adds GameFragment

--- app/src/main/java/com/example/wordgusser/ui/title/TitleFragment.kt
+++ app/src/main/java/com/example/wordgusser/ui/title/TitleFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
 import com.example.wordgusser.R
 import com.example.wordgusser.databinding.FragmentTitleBinding
 
@@ -19,6 +20,9 @@ class TitleFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         binding = DataBindingUtil.inflate(inflater,R.layout.fragment_title, container, false)
+        binding.playButton.setOnClickListener { view : View ->
+            view.findNavController().navigate(R.id.action_titleFragment_to_gameFragment)
+        }
         return binding.root
     }
 
